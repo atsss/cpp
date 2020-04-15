bfe46a843e41ebea46b564eea19d68acc8ede0fd
atsss <atsushi0623itoh@gmail.com>
2020-04-14 19:20:56 -0600

    fix basic files

--- basic/music.cpp
+++ basic/music.cpp
@@ -5,7 +5,7 @@ int main() {
 
   Song back_to_black("Back to Black", "Amy Winehouse");
 
-  std::cout << back_to_black.get_title();
-  std::cout << back_to_black.get_artist();
+  std::cout << back_to_black.get_title() << "\n";
+  std::cout << back_to_black.get_artist() << "\n";
 
 }
