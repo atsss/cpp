de6fc552606e51e144d8f989a713be5bd12d7dc7
atsss <atsushi0623itoh@gmail.com>
2020-07-14 09:05:00 -0600

    create 8-4 answer

---
+++ quetions/8/4/main.cpp
@@ -0,0 +1,51 @@
+#include <vector>
+#include <iostream>
+using namespace std;
+
+vector<vector<int>> getSubsets(vector<int> set, int index) {
+  vector<vector<int>> allSubsets;
+
+  if (set.size() == index) {
+    vector<int> emptySubset = {};
+    allSubsets.push_back(emptySubset);
+  } else {
+    allSubsets = getSubsets(set, index + 1);
+
+    int item = set[index];
+    vector<vector<int>> moreSubsets = {};
+
+    for (int i = 0; i < allSubsets.size(); i++) {
+      vector<int> subsets = allSubsets[i];
+      vector<int> newSubsets = subsets;
+
+      newSubsets.push_back(item);
+
+      moreSubsets.push_back(newSubsets);
+    }
+
+    allSubsets.insert(allSubsets.end(), moreSubsets.begin(), moreSubsets.end());
+  }
+
+  return allSubsets;
+}
+
+int main() {
+  vector<int> set = { 100, 200, 300 };
+
+  vector<vector<int>> allSubsets;
+  allSubsets = getSubsets(set, 0);
+
+  for (int i = 0; i < allSubsets.size(); i++) {
+    vector<int> subsets = allSubsets[i];
+
+    cout << "{ ";
+    for (int j = 0; j < subsets.size(); j++) {
+        cout << allSubsets[i][j];
+        // cout << subsets[i]; # 51L がポインタの交換になっているのでうまく行かない
+        if (j != subsets.size() - 1) { cout << ", "; };
+    }
+    cout << " }" << endl;
+  }
+
+  return 0;
+}
